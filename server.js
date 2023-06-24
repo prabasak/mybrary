c30446cf92703816ef807d5b0e713ca5a2b50c4c
Pratik Basak <pratik.basak@publicissapient.com>
2023-06-24 17:08:05 +0530

    Third initial commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static('public'))
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
-db.on('error', () => {console.error(error)})
+db.on('error', (error) => {console.error(error)})
 db.once('open', () => { console.log('Connected to mongoose')})
 
 app.use('/', indexRouter)
